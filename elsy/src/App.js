ac8b451bf218c9f799e8e9522a1cc5efa297b6e8
Houra-Am <61083815+Houra-Am@users.noreply.github.com>
2021-01-26 15:42:38 +0100

    elsy part 1

--- elsy/src/App.js
+++ elsy/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import "./components/Steps.jsx"
+import "./components/Box.jsx"
 import './App.css';
-import Steps from "./components/Steps.jsx";
+import "./styles/global.css"
+import Box from "./components/Box.jsx";
 const tempMin = -20;
 const tempMax = 40;
 const heartMin = 80;
@@ -17,7 +18,14 @@ export class App extends React.Component {
             <p>heart: {heartMin}</p>
             <p>Temperature: {tempMin}</p>
             <p>Steps: {stepsMin}</p>
-            <Steps></Steps>
+            {/* Water*/}
+            <Box icon={"local_drink"} color= "#3A85FF" value = {1.5} unit = "L"> </Box>
+            {/* Steps*/}
+            <Box icon={"directions_walk"} color= "black" value = {3000} unit = "steps"></Box>
+            {/* Heart*/}
+            <Box icon={"favorite"} color= "red" value = {120} unit = "bpm"></Box>
+            {/* Temperature */}
+            <Box icon={"wb_sunny"} color= "yellow" value = {-10} unit = "°C"></Box>
         </div>
       </div>
     );
